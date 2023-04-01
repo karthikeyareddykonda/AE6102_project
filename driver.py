b7e650ce3c0c10cc21f7409f555e7953bf4a5a4c
Akash Reddy G <akash.gillella@gmail.com>
2023-04-01 22:52:34 +0530

    Self updated in method

--- driver.py
+++ driver.py
@@ -1,24 +1,20 @@
 
 
-
-
-
 if __name__ == "__main__":
 
     '''
     options
-    Display while simulation being run ? ---- can show the difference in simulation speeds of differetn optimizations
-    Display after simulation done for limited time.. Can Either do it in a fixed simulation time, or real time
+    Display while simulation being run ? ---- can show the difference 
+    in simulation speeds of differetn optimizations
+    Display after simulation done for limited time.. Can Either do it in 
+    a fixed simulation time, or real time
 
     scale ?   
     No of objects ?
     Size of interval simulations ?
 
-    What to simulate ? Simulate the questions for which answers are solved by simulations... :) 
-    
-     
+    What to simulate ? Simulate the questions for which answers are solved by 
+    simulations... :) 
+
+
     '''
-    
-
-
-
--- object.py
+++ object.py
@@ -1,26 +1,27 @@
 
 G = 6.67*1e-11
-def gravity(m1,m2,x1,y1,z1,x2,y2,z2):
+
+
+def gravity(m1, m2, x1, y1, z1, x2, y2, z2):
 
     #  resolved gravity ?
-    
+
     r = ((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)**0.5
     C = G*m1*m2/(r**3)
     fx = C*(x1-x2)
     fy = C*(y1-y2)
     fz = C*(z1-z2)
 
-    return fx,fy,fz
-
+    return fx, fy, fz
 
 
 class object:
 
-    x,y,z,vx,vy,vz = 0,0,0,0,0,0
+    x, y, z, vx, vy, vz = 0, 0, 0, 0, 0, 0
     m = 0
 
-    def __init__(self,m =1 ,x=0, y=0, z=0,vx=0,vy=0,vz=0) :
-        
+    def __init__(self, m=1, x=0, y=0, z=0, vx=0, vy=0, vz=0):
+
         self.m = m
         self.x = x
         self.y = y
@@ -29,19 +30,15 @@ class object:
         self.vy = vy
         self.vz = vz
 
+    def update(self, dt, ax, ay, az):
 
-    def update(dt,ax,ay,az):
+        self.vx = self.vx + ax*dt
+        self.vy = self.vy + ay*dt
+        self.vz = self.vz + az*dt
 
-        vx = vx + ax*dt
-        vy = vy + ay*dt
-        vz = vz + az*dt
-        
         # Can use complex methods
         # ut + (1/2)a t*t
-        # 
-        x = x + vx*dt
-        y = y + vy*dt
-        z = z + vz*dt
-
-
-
+        #
+        self.x = self.x + self.vx*dt
+        self.y = self.y + self.vy*dt
+        self.z = self.z + self.vz*dt
